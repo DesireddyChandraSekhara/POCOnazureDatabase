179d5bf7f471ff0c2d45944b63a230da3cba70e4
DesireddyChandraSekhara <83830788+DesireddyChandraSekhara@users.noreply.github.com>
2021-06-05 14:57:14 +0530

    Created New Table (Tbl_Studnet)

---
+++ ChanduDotNetDBProject/tbl_Student.sql
@@ -0,0 +1,9 @@
+insert into tbl_Student(ID,Name,Age,Gender,DOB,IsActive,CreatedOn)
+VALUES(4,'Student 4',20,'M','2000-01-01',1,GetDate()),
+(2,'Student 2',19,'F','2001-01-01',1,GetDate()),
+(3,'Student 3',19,'M','2001-01-01',1,GetDate())
+
+
+
+
+select * from tbl_Student
